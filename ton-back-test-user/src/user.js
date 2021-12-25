66e140b45bebda6e6a3ea72c2c538ce90071865e
matheuspsantos <msantos.2015@alunos.utfpr.edu.br>
2021-12-25 10:58:30 -0300

    feat: adicionando criptografia da senha

--- ton-back-test-user/src/user.js
+++ ton-back-test-user/src/user.js
@@ -1,11 +1,12 @@
 const AWS = require("aws-sdk");
 const { v4 } = require("uuid");
 
+const TABLE_NAME = "UserTable";
+
 const createUser = async (event) => {
     const dynamo = new AWS.DynamoDB.DocumentClient();
-
     const { username, email, password } = JSON.parse(event.body);
-
+    const hashed_password = hashPassword(password);
     const createdAt = new Date();
     const id = v4();
 
@@ -13,12 +14,13 @@ const createUser = async (event) => {
         id,
         username,
         email,
-        password, // precisa fazer a cryto desse password
+        hashed_password,
         createdAt
     };
+
     try {
-        dynamo.put({
-            TableName: "User",
+        await dynamo.put({
+            TableName: TABLE_NAME,
             Item: newUser
         }).promise();
 
@@ -29,6 +31,10 @@ const createUser = async (event) => {
 
     } catch (error) {
         console.error("Create user Error: ", error);
+        return {
+            statusCode: 500,
+            body: "CreateUserExeception: Internal Server Error."
+        }
     }
 };
 
@@ -36,12 +42,20 @@ const listUser = async (event) => {
     const dynamo = new AWS.DynamoDB.DocumentClient();
     let user;
     try {
-        const results = await dynamo.scan({ TableName: "User " }).promise(); // preciso fazer a busca por ID específico
+        const results = await dynamo.scan({
+            TableName: TABLE_NAME
+        }).promise(); // preciso fazer a busca por ID específico
         // to do: estudar query no dynamo
         user = results.Items;
     } catch (error) {
         console.error("List User Error: ", error);
     }
 };
+
+const hashPassword = (pass) => {
+    const { createHash } = require("crypto");
+    const hash = createHash("sha512");
+    return hash.update(pass).digest("hex");
+}
 
 module.exports = { createUser, listUser }
